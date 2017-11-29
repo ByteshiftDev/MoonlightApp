0fcbd75e387a7d53d6bddafc0bdec3fa79372305
jasoncustodio <jmaxcustodio@gmail.com>
2017-11-29 12:41:27 -0800

    Fixed Style

--- components/Style.js
+++ components/Style.js
@@ -1,64 +1,49 @@
 import { StyleSheet } from "react-native";
 
 const Style = StyleSheet.create({
-    myView: {
-      marginTop: 20,
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-      backgroundColor: '#F5FCFF',
-    },
+  myView: {
+    marginTop: 10,
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF"
+  },
 
-    myText: {
-      fontSize: 20,
-      textAlign: 'center',
-      margin: 300,
-    },
+  myText: {
+    fontSize: 15,
+    textAlign: "center",
+    margin: 10
+  },
 
-    homeView: {
-      flex: 1,
-      // justifyContent: 'center',
-      alignItems: 'center',
-      backgroundColor: '#212121',
-    },
+  homeView: {
+    flex: 1,
+    // justifyContent: 'center',
+    alignItems: "center",
+    backgroundColor: "#212121"
+  },
 
-    logoView: {
-      flex: 0.75,
-      justifyContent: 'center',
-      alignItems: 'center',
-      backgroundColor: '#212121',
-    },
+  logoView: {
+    flex: 0.75,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#212121"
+  },
 
-    homeLogo: {
-      flex: 0.9,
-      aspectRatio: 1.0,
-      resizeMode: 'contain',
-    },
+  homeLogo: {
+    flex: 0.9,
+    aspectRatio: 1.0,
+    resizeMode: "contain"
+  },
 
-    homeButton: {
-      marginTop: 20,
-      fontFamily: 'Montserrat',
-      padding: 15,
-      backgroundColor: '#FAFCED',
-      color: '#212121',
-      fontSize: 12,
-      letterSpacing: 0.1,
-    },
-
-    myView: {
-      marginTop: 10,
-      flex: 1,
-      justifyContent: "center",
-      alignItems: "center",
-      backgroundColor: "#F5FCFF"
-    },
-  
-    myText: {
-      fontSize: 15,
-      textAlign: "center",
-      margin: 10
-    }
-
-  });
+  homeButton: {
+    marginTop: 20,
+    fontFamily: "Montserrat",
+    padding: 15,
+    backgroundColor: "#FAFCED",
+    color: "#212121",
+    fontSize: 12,
+    letterSpacing: 0.1
+  }
+});
 
 export default Style;
